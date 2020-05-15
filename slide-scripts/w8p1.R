68013b7426e8db14fb7fb0f9c80dd8f66973b385
Daniel Anderson <daniela@uoregon.edu>
2020-05-15 12:50:02 -0700

    post w8 slides

---
+++ slide-scripts/w8p1.R
@@ -0,0 +1,8 @@
+## ----setup, include = FALSE------------------------------------------------------------------------------
+library(tidyverse)
+theme_set(theme_minimal(20))
+update_geom_defaults("point", list(size = 3))
+knitr::opts_chunk$set(fig.width = 13, 
+                      message = FALSE, 
+                      warning = FALSE)
+
